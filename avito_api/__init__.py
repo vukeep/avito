6f3d8e5d690c0137bc06aec8914b2e7b971451a4
vukeep <vukeep@gmail.com>
2025-03-11 14:21:16 +0900

    fix

--- avito_api/__init__.py
+++ avito_api/__init__.py
@@ -3,7 +3,7 @@
 Оптимизирован для быстрой интеграции и высокой производительности
 """
 
-from .api_client import AvitoApiClient
+from .api_client import AvitoAPIClient  # Было AvitoApiClient, меняем на AvitoAPIClient 
 from . import utils
 from . import user
 from . import services_item
@@ -12,7 +12,7 @@ from . import item
 from . import auth
 
 # Экспортируем основной класс для удобного импорта
-__all__ = ['AvitoApiClient']
+__all__ = ['AvitoAPIClient']  # Также меняем здесь
 
 # Версия пакета
 __version__ = '1.0.0'
